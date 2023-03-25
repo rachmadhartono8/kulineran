399a4acd8452e92a1067115d79936ca8c79d53f9
rachmadhartono8 <rahmadhartono62@gmail.com>
2023-03-25 23:29:53 +0700

    Winahyu Homestay

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,30 +2,30 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      Lagi Belajar Front End Developer Mohon Bimbingan nya Kak,<br>
+      Ini Lagi ngembangin Website pake Vue JS
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">Lihat beberapa gambar homestay kami</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Find Winahyu Homestay at Traveloka/Booking.com/Tiket.com</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">Winahyu Homestay</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">Winahyu Resort Kukup</a></li>
     </ul>
-    <h3>Essential Links</h3>
+    <h3>Staycation dengan sensasional pantai</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Traveloka</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Booking.com</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Agoda</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Tiket.com</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">Instagram</a></li>
     </ul>
-    <h3>Ecosystem</h3>
+    <h3>booking sekarang</h3>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">Book Now at Traveloka</a></li>
+      <!-- <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li> -->
     </ul>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App Buatan Hartono"/>
+    <HelloWorld msg="Selamat datang di Winahyu Resort Kukup"/>
   </div>
 </template>
 
